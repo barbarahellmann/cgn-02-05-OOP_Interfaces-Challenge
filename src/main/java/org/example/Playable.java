f75657606a33d5deb28398d19def56b4a0ab67a2
barbarahellmann <babshellmann@web.de>
2024-03-18 12:42:58 +0100

    Wording fix

--- src/main/java/org/example/Playable.java
+++ src/main/java/org/example/Playable.java
@@ -2,5 +2,5 @@ package org.example;
 
 public interface Playable {
 
-    public void play();
+    void play();
 }
--- src/main/java/org/example/VideoPlayer.java
+++ src/main/java/org/example/VideoPlayer.java
@@ -5,6 +5,5 @@ public class VideoPlayer implements Playable{
     @Override
     public void play() {
         System.out.println("Play a video");
-
     }
 }
